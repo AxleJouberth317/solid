e258c6e66a1d4adf95d28aa41f84f94f2bf5140e
Cliente CONTROL-PC <CltControl@GGIECUT1WRK027.espol.edu.ec>
2018-06-05 13:12:45 -0500

    bien dip

---
+++ solid/src/_4dip/bien/Programador.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _4dip.bien;
+
+import _4dip.bien.*;
+
+/**
+ *
+ * @author Palacios
+ */
+public class Programador {
+    
+    private Computadora computadora;
+    
+    /*
+        Computador cualquiera
+    */
+    public Programador() {
+        this.computadora = new Computadora();
+    }
+
+    public Computadora getComputadora() {
+        return computadora;
+    }
+
+    public void setComputadora(Computadora computadora) {
+        this.computadora = computadora;
+    }
+
+    
+    
+    
+    
+}
